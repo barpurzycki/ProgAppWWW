8503a9d5050816479467e15ab2d8eb08d951e40c
barpurzycki <92019696+barpurzycki@users.noreply.github.com>
2023-11-11 12:13:01 +0100

    Add files via upload

--- lab5/index.php
+++ lab5/index.php
@@ -3,14 +3,14 @@
 
     /* po tym komentarzu będzie kod do dynamicznego ładowania stron */
 
-    if($_GET['idp']=='')$strona = '../lab4/html/glowna.html';
-    if($_GET['idp']=='cyberpunk')$strona = '../lab4/html/cyberpunk.html';
-    if($_GET['idp']=='ff14')$strona = '../lab4/html/finalfantasy.html';
-    if($_GET['idp']=='eldenring')$strona = '../lab4/html/eldenring.html';
-    if($_GET['idp']=='ds')$strona = '../lab4/html/darksouls.html';
-    if($_GET['idp']=='tw3')$strona = '../lab4/html/witcher.html';
-    if($_GET['idp']=='kontakt')$strona = '../lab4/html/kontakt.html';
-    if($_GET['idp']=='filmy')$strona = '../lab4/html/filmy.html';
+    if($_GET['idp']=='')$strona = '../lab5/html/glowna.html';
+    if($_GET['idp']=='cyberpunk')$strona = '../lab5/html/cyberpunk.html';
+    if($_GET['idp']=='ff14')$strona = '../lab5/html/finalfantasy.html';
+    if($_GET['idp']=='eldenring')$strona = '../lab5/html/eldenring.html';
+    if($_GET['idp']=='ds')$strona = '../lab5/html/darksouls.html';
+    if($_GET['idp']=='tw3')$strona = '../lab5/html/witcher.html';
+    if($_GET['idp']=='kontakt')$strona = '../lab5/html/kontakt.html';
+    if($_GET['idp']=='filmy')$strona = '../lab5/html/filmy.html';
 ?>
 
 <!DOCTYPE HTML>
@@ -25,6 +25,18 @@
         </title>
     </head>
     <body style="background-image: url('Pictures/background.jpg');">
+        <div id="menu">
+            <ul>
+                <li><b><a href="../lab5/index.php">Strona główna</a></b></li>
+                <li><b><a href="../lab5/index.php?idp=cyberpunk">Cyberpunk 2077</a></b></li>
+                <li><b><a href="../lab5/index.php?idp=ff14">Final Fantasy 14</a></b></li>
+                <li><b><a href="../lab5/index.php?idp=eldenring">Elden Ring</a></b></li>
+                <li><b><a href="../lab5/index.php?idp=ds">Dark Souls</a></b></li>
+                <li><b><a href="../lab5/index.php?idp=tw3">The Witcher 3</a></b></li>
+                <li><b><a href="../lab5/index.php?idp=kontakt">Kontakt</a></b></li>
+                <li><b><a href="../lab5/index.php?idp=filmy">Filmy</a></b></li>
+            </ul>
+        </div>
         <?php
             include($strona);
         ?>
