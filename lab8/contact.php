fedfa7c92c7819b52a3a565aed6af42e7d6d7362
barpurzycki <92019696+barpurzycki@users.noreply.github.com>
2023-11-30 16:36:06 +0100

    Add files via upload

--- lab8/contact.php
+++ lab8/contact.php
@@ -1,16 +1,42 @@
 <?php
 
+session_start();
+
+$dbhost = 'localhost';
+$dbuser = 'root';
+$dbpass = '';
+$baza = 'moja_strona';
+
+$link = mysqli_connect($dbhost, $dbuser, $dbpass);
+if(!$link) echo '<b>Połączenie przerwane </b>';
+if(!mysqli_select_db($link, $baza)) echo 'Nie wybrano bazy danych';
+
 function PokazKontakt()
 {
-
+	$wynik= '
+	<div class="kontakt">
+	    <h1 class="heading">Formularz kontaktowy</h1>
+        <div class="kontakt">
+	        <form method="post" name="ContactForm" enctype-"multipart/form-data" action="'. $_SERVER['REQUEST_URI'].'">
+	            <table class="kontakt">
+		        <tr><td class="contact_form">Temat: </td><td><input type="text" name="temat" class="kontakt" /></td></tr>
+		        <tr><td class="contact_form">Treść: </td><td><input type="text" name="tresc" class="kontakt" /></td></tr>
+                <tr><td class="contact_form">Email: </td><td><input type="text" name="email" class="kontakt" /></td></tr>
+                <tr><td>&nbsp;</td><td><input type="submit" name="x1_submit" class="kontakt" value="Wyślij" /></td></tr>
+		        </table>
+	        </form>
+	    </div>
+	</div>
+	';
+	
+	return $wynik;
 }
 
-function WyslijMailaKontakt($odbiorca)
+function WyslijMailKontakt($odbiorca)
 {
     if(empty($_POST['temat']) || empty($_POST['tresc']) || empty($_POST['email']))
     {
         echo '[nie_wypelniles_pola]';
-        echo PokazKontakt();
     }
     else
     {
@@ -26,7 +52,7 @@ function WyslijMailaKontakt($odbiorca)
         $header .= "X-Priority: 3\n";
         $header .= "Return-Path: <".$mail['sender'].">\n";
 
-        mail($mail['reciptient'].$mail['subject'].$mai['body'].$header);
+        mail($mail['reciptient'],$mail['subject'],$mail['body'],$header);
 
         echo '[wiadomosc_wyslana]';
     }
@@ -37,6 +63,17 @@ function PrzypomnijHaslo()
 
 }
 
+
+
+if (isset($_SESSION['status'])) 
+{
+
+} 
+else 
+{
+    echo PokazKontakt();
+    WyslijMailKontakt($odbiorca);
+}
 
 
 ?>
