2fe087003a359a139b0eb2696e7d587fcdd199ff
barpurzycki <92019696+barpurzycki@users.noreply.github.com>
2024-01-17 21:08:09 +0100

    Delete lab12 directory

--- lab12/order.php
+++
@@ -1,5 +0,0 @@
-<link rel="stylesheet" href="css/orderstyles.css">
-<h1>Udało Ci się złożyć zamówienie!</h1>
-<form method="post" action="sklep.php">
-    <input type="submit" value="Wróć do sklepu">
-</form>
